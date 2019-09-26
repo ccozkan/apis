0c2123170709bad7dbb3e79e741a2ffb704e6b13
Çağrı Özkan <cagrio.dev@gmail.com>
2019-09-26 04:06:19 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'welcome#index'
+  get '/antennas/city'
+  get '/antennas/band'
+  get '/antennas/city_band'
+  resources :antennas, only: [:index, :show]
+
+
 end
