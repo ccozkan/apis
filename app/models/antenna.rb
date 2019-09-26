0fb508f80ad92c570ae99bd1a355bf192699a004
Çağrı Özkan <cagrio.dev@gmail.com>
2019-09-26 04:04:09 +0300

    Update antennas model for passing arguments

--- app/models/antenna.rb
+++ app/models/antenna.rb
@@ -1,2 +1,9 @@
 class Antenna < ApplicationRecord
+
+  #scope :APRS, -> {Antenna.where("band = 'APRS'")}
+
+  scope :band_type, -> (band) {Antenna.where(band: band)}
+  scope :city_name, -> (city) {Antenna.where(city: city)}
+  scope :city_and_band, -> (city,band) {Antenna.where(city: city, band: band)}
+
 end
