45193d66fded3d2bf19e5ed84dec0c924e6aa191
Çağrı Özkan <cagrio.dev@gmail.com>
2019-12-16 21:22:11 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'welcome#index'
+
   get '/antennas/city'
   get '/antennas/band'
   get '/antennas/city_band'
   resources :antennas, only: [:index, :show]
 
   get '/beverages/name'
+  get '/beverages/category'
   resources :beverages, only: [:index, :show]
+
+  resources :radio_stations, only: [:index, :show]
 end
